665b45972d8d318e7492693a2ef9d730481dfccf
fanqingsong <fanqingsong@ghostcloud.cn>
2018-03-15 20:01:10 +0800

    add govendor

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"pybbs-go/models"
+	_ "pybbs-go/routers"
+	_ "pybbs-go/templates"
+	_ "pybbs-go/utils"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func init() {
+	orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("jdbc.username")+":"+beego.AppConfig.String("jdbc.password")+"@tcp(192.168.34.145)/pybbsgo?charset=utf8&parseTime=true&charset=utf8&loc=Asia%2FShanghai", 30)
+	orm.RegisterModel(
+		new(models.User),
+		new(models.Topic),
+		new(models.Section),
+		new(models.Reply),
+		new(models.ReplyUpLog),
+		new(models.Role),
+		new(models.Permission))
+	orm.RunSyncdb("default", false, true)
+}
+
+func main() {
+	//orm.Debug = true
+	//ok, err := regexp.MatchString("/topic/edit/[0-9]+", "/topic/edit/123")
+	//beego.Debug(ok, err)
+	beego.Run()
+}
